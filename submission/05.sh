8560393c96ccc55002ad45fd0a85422321315419
72kb <lanniox@gmail.com>
2026-03-17 08:27:28 +0100

    05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -1,1 +1,9 @@
-bitcoin-cli -regtest getbalance
+#!/bin/bash
+# 1. Generate an address to receive the reward
+ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+
+# 2. Mine 101 blocks to that address to make the reward spendable
+bitcoin-cli -regtest -rpcwallet=builderswallet generatetoaddress 101 $ADDR > /dev/null
+
+# 3. Now check the balance (it should be 50.0)
+bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
