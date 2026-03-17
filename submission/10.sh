b6c34a68def3eb1d1beca77cc94751ae1fc11cb3
72kb <lanniox@gmail.com>
2026-03-17 08:31:37 +0100

    10.sh

--- submission/10.sh
+++ submission/10.sh
@@ -1,1 +1,2 @@
-bitcoin-cli -regtest validateaddress bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy
+#!/bin/bash
+bitcoin-cli -regtest validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" | jq -r .isvalid
